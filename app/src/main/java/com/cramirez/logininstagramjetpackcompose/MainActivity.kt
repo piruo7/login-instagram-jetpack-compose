fc0db2ad981011b32309f3f28bd8cee61b457413
Christian Ramirez <christian.ramirez.marinos@gmail.com>
2023-06-30 22:19:18 -0400

    UI - Login Instagram

---
+++ app/src/main/java/com/cramirez/logininstagramjetpackcompose/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.cramirez.logininstagramjetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.cramirez.logininstagramjetpackcompose.ui.theme.LoginInstagramJetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LoginInstagramJetpackComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LoginScreen()
+                }
+            }
+        }
+    }
+}
